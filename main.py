d8d42bf17e3dee72026db86580b5c712b56874b0
Alottachairs <adonfields@gmail.com>
2022-06-14 23:53:50 -0500

    updates

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from kivy.app import App
+from kivy.uix.widget import Widget
+
+class SlotGame(Widget):
+
+    pass
+
+class SlotApp(App):
+    def build(self):
+        game = SlotGame()
+        return game
+
+if __name__ == '__main__':
+    SlotApp().run()
